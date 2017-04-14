6df60a666e2a311e87a68431905171f2081296ca
chaluvadis <surendra.chaluvadi@gmail.com>
2017-04-14 20:31:42 +0400

    task data source added

---
+++ generate-todo.js
@@ -0,0 +1,13 @@
+module.exports = function(){
+    var faker = require('faker');
+    var _ = require('lodash');
+    return {
+       task : _.times(10, (n)=>{
+            return {
+                id: n,
+                title: faker.company.companyName(),
+                status: faker.random.arrayElement(["pending", "done","offline"])
+            }
+        })
+    }
+}
