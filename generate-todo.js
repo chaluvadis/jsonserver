850de5fe4cf9f1c3562d4bb21ce554cce1e4ecac
chaluvadis <surendra.chaluvadi@gmail.com>
2017-04-16 20:54:48 +0400

    unnecssary files are removed

--- generate-todo.js
+++ generate-todo.js
@@ -1,12 +1,12 @@
-module.exports = function(){
+module.exports = function () {
     var faker = require('faker');
     var _ = require('lodash');
     return {
-       task : _.times(10, (n)=>{
+        task: _.times(10, (n) => {
             return {
-                id: n,
+                id: n + 1,
                 title: faker.company.companyName(),
-                status: faker.random.arrayElement(["pending", "done","offline"])
+                status: faker.random.arrayElement(["pending", "done", "offline"])
             }
         })
     }
