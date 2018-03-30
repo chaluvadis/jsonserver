673abe129fd384a86afe4ff255292778b5423d17
chaluvadis <surendra.chaluvadi@gmail.com>
2018-03-29 23:14:02 -0500

    employees list added

--- generate-assignments.js
+++ generate-assignments.js
@@ -8,7 +8,8 @@ module.exports = function() {
                 clientname: faker.name.jobTitle(),
                 assignmentguid: faker.random.uuid(),
                 questassignmentid: faker.random.number(100000, 999999).toString() + '-' + faker.random.number(1000, 9999).toString(),
-                isActive: true
+                isActive: true,
+                createdDate: faker.date.recent(),
             }
         })
     }
